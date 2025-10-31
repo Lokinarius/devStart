f29e882ac10e6564a920e4a86ae980969acb13f1
William Galvão <Liam_draven@outlook.com>
2025-10-31 15:51:46 -0300

    Create dadosAtletas.js

---
+++ trilha01/projeto02/dadosAtletas.js
@@ -0,0 +1,53 @@
+// Array de objetos representando os dados dos atletas
+export let atletas = [
+    {
+        nome: "João Silva",
+        idade: 25,
+        peso: 70,
+        altura: 1.75,
+        notas: [9.5, 8.7, 9.0, 8.5, 9.2]
+    },
+    {
+        nome: "Maria Souza",
+        idade: 22,
+        peso: 60,
+        altura: 1.65,
+        notas: [8.0, 7.5, 8.3, 8.7, 9.0]
+    },
+    {
+        nome: "Carlos Pereira",
+        idade: 30,
+        peso: 80,
+        altura: 1.80,
+        notas: [9.8, 9.6, 9.7, 9.5, 9.9]
+    },
+    {
+        nome: "Ana Costa",
+        idade: 15,
+        peso: 55,
+        altura: 1.60,
+        notas: [7.5, 7.8, 8.0, 7.9, 8.2]
+    },
+    {
+        nome: "Pedro Alves",
+        idade: 10,
+        peso: 40,
+        altura: 1.40,
+        notas: [8.5, 8.7, 8.9, 9.0, 8.8]
+    },
+    {
+        nome: "Luiza Fernandes",
+        idade: 13,
+        peso: 50,
+        altura: 1.55,
+        notas: [9.0, 9.2, 9.1, 9.3, 9.4]
+    },
+    {
+        nome: "Rafael Gomes",
+        idade: 14,
+        peso: 65,
+        altura: 1.70,
+        notas: [7.0, 7.2, 7.5, 7.3, 7.4]
+    }
+]
+
